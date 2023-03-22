a6177bff0a1dc838be7fd9f60b683476208e4c06
Thomas O'Neill <oneill@ruc.dk>
2023-03-22 22:23:15 +0100

    Moved saved_to_db inside the Drinker class

--- main.py
+++ main.py
@@ -16,19 +16,39 @@ class Drinker:
     max_bac: float
     drive_time: datetime
 
-    def minutes_since_start_time(self):
+    def minutes_since_start_time(self) -> int:
         time_diff = datetime.now() - self.start_time
         return round(time_diff.total_seconds() / 60)
 
-    def minutes_until_drive_time(self):
+    def minutes_until_drive_time(self) -> int:
         if self.drive_time:
             time_diff = self.drive_time - datetime.now()
             return round(time_diff.total_seconds() / 60)
         else:
             return
 
+    def save_to_db(self):
+        drive_time = self.drive_time.isoformat() if self.drive_time else None
+        drinkers[self.dob] = {
+            "mode": self.mode,
+            "sex": self.sex,
+            "weight": self.weight,
+            "start_time": self.start_time.isoformat(),
+            "max_bac": self.max_bac,
+            "drive_time": self.drive_time.isoformat() if self.drive_time else None,
+        }
+        try:
+            with open("drinkers.json", "w") as outfile:
+                json.dump(drinkers, outfile)
+        except Exception:
+            print("Unable to load database file")
+        return drinkers
 
-def get_drinker(dob: int):
+
+def get_drinker(dob: int) -> Drinker:
+    if dob not in drinkers:
+        return
+
     d = drinkers[dob]
     start_time = datetime.fromisoformat(d["start_time"])
     drive_time = datetime.fromisoformat(d["drive_time"]) if d["drive_time"] else None
@@ -44,24 +64,11 @@ def get_drinker(dob: int):
     return drinker
 
 
-def save_drinker(drinker: Drinker) -> dict:
-    drive_time = drinker.drive_time.isoformat() if drinker.drive_time else None
-    drinkers[drinker.dob] = {
-        "mode": drinker.mode,
-        "sex": drinker.sex,
-        "weight": drinker.weight,
-        "start_time": drinker.start_time.isoformat(),
-        "max_bac": drinker.max_bac,
-        "drive_time": drive_time,
-    }
-    with open("drinkers.json", "w") as outfile:
-        json.dump(drinkers, outfile)
-    return drinkers
-
-
-drinker = get_drinker(dob="200301028")
+drinker = get_drinker(dob="20031028")
 print(drinker.mode)
 print(drinker.start_time)
 print(drinker.drive_time)
 print(drinker.minutes_since_start_time())
 print(drinker.minutes_until_drive_time())
+
+drinker.save_to_db()
