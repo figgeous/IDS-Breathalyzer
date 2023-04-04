4e65840c4c4fbf7c6a7f11612a11317144f7d153
Motto <michaeltekleotto@gmail.com>
2023-04-04 20:25:04 +0200

    Found a different solution instead of localview

--- main.py
+++ main.py
@@ -2,6 +2,28 @@ import os
 from random import randint
 
 import eel
+import http.server
+import socketserver
+import os
+import qrcode
+
+
+# Generate a QR code with the local host URL
+url = 'http://localhost:8000'
+img = qrcode.make(url)
+
+# Save the QR code image to a file
+img.save('qrcode.png')
+
+# Define the web server handler
+handler = http.server.SimpleHTTPRequestHandler
+
+# Start the web server on port 8000
+with socketserver.TCPServer(("", 8000), handler) as httpd:
+    print("Server started at http://localhost:8000")
+    # Wait for incoming connections
+    httpd.serve_forever()
+
 
 dirname = os.path.dirname(__file__)
 eel.init(dirname + "/web/")
