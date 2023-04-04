b7b0fd1d355975b7bbcb68d4a7ee0aa0a0a924ee
Rickie19999 <114414456+Rickie19999@users.noreply.github.com>
2023-04-04 22:11:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-import os
-from random import randint
+from flask import Flask, render_template, request, app
+import json
 
-import eel
+app = Flask(__name__)
 
-dirname = os.path.dirname(__file__)
-eel.init(dirname + "/web/")
+@app.route('/')
+def index():
+    return render_template('create_account.html')
 
+@app.route('/register', methods=['POST'])
+def register():
+    # Load existing user data
+    import os
 
-# Exposing the random_python function to javascript
-@eel.expose
-def random_python():
-    print("Random function running")
-    return randint(1, 100)
+    if not os.path.exists('users.json'):
+        with open('users.json', 'w') as f:
+            f.write('[]')
 
+    with open('users.json', 'r') as f:
+        data = json.load(f)
 
-eel.start("index.html", mode="firefox")
+    username = request.form['username']
+    password = request.form['password']
+    dob = request.form['dob']
+
+    # Check if username already exists
+    if any(user['username'] == username for user in data):
+        return 'Username already exists!'
+
+    # Add new user to data
+    data.append({'username': username, 'password': password, 'dob': dob})
+    with open('users.json', 'w') as f:
+        json.dump(data, f)
+
+    return f'Account created successfully! Username: {username}, Date of Birth: {dob}'
+
+if __name__ == '__main__':
+    app.run()
